61f2ed6170e63b859e72a0d5580eac76b3648dbc
Bllasnows <bellaneves28@gmail.com>
2024-06-03 15:40:59 -0300

    Aula de estruturas de repetição

---
+++ Estruturas de repetição/Repeticao/src/ExemploForArrays.java
@@ -0,0 +1,11 @@
+public class ExemploForArrays {
+    public static void main(String[] args) {
+
+        String alunos[] = {"FELIPE", "JONAS", "JULIA", "MARCOS"};
+
+    // Em arrays, o indice de elementos inicia em 0
+        for (int x=0; x<alunos.length; x++){
+            System.out.println("O aluno no indice x= " + x + " é " + alunos[x]);
+        }
+    }
+}
